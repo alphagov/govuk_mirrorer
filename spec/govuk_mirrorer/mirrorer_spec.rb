bd3768497e926e14bf5621fff406d2cf2d2e8cf8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-04-29 10:11:16 +0100

    Fix deprecation warnings in tests.

--- spec/govuk_mirrorer/mirrorer_spec.rb
+++ spec/govuk_mirrorer/mirrorer_spec.rb
@@ -5,7 +5,7 @@ describe GovukMirrorer do
   describe "top-level run method" do
     it "should construct a new crawler with the configuration, and start it" do
       GovukMirrorer::Configurer.should_receive(:run).and_return(:a_config_hash)
-      stub_crawler = stub("GovukMirrorer::Crawler")
+      stub_crawler = double("GovukMirrorer::Crawler")
       GovukMirrorer::Crawler.should_receive(:new).with(:a_config_hash).and_return(stub_crawler)
       stub_crawler.should_receive(:crawl)
 
